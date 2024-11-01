085cb1d19544c6ecc1afad7c0da1acaf82edc05c
izzytoot <isabeltootill@gmail.com>
2024-11-01 20:05:11 +0000

    ft_putstr_fd

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -1,41 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/01 14:40:57 by icunha-t          #+#    #+#             */
-/*   Updated: 2024/11/01 19:54:21 by root             ###   ########.fr       */
+/*   Created: 2024/11/01 18:35:56 by root              #+#    #+#             */
+/*   Updated: 2024/11/01 20:04:59 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	if (!s)
 		return ;
-	write(fd, s, ft_strlen(s));
+	write (fd, s, (ft_strlen(s)));
+	write (fd, "\n", 1);
 }
 /*
+#include <stdio.h>
 #include <fcntl.h>
-#include "libft.h"
 
-int main(void)
+int	main(void)
 {
-    int	fd;
+	int	fd;
 	char	*test_st;
 	char	*test_sf;
-
-	fd = open("output_str.txt", O_WRONLY | O_CREAT, 0644);
+	
+	fd = open("output_endl.txt", O_WRONLY | O_CREAT, 0644);
 	if (fd == -1)
         return (1);
-	test_st = "Test terminal";
-	ft_putstr_fd(test_st, 1);
 	test_sf = "Test file";
-	ft_putstr_fd(test_sf, fd);
-    close(fd);
+	ft_putendl_fd (test_sf, 1);
+	test_st = "Test terminal";
+	ft_putendl_fd (test_sf, fd);
+	close(fd);
 	return (0);
 }
 */
